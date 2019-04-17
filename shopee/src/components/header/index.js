8811ea405098a4ad4278e04ef048ce75ba849a33
Guest User <Guest@onyemaechismbp2.lan>
2019-04-17 15:02:16 +0100

    Created a header

--- shopee/src/components/header/index.js
+++ shopee/src/components/header/index.js
@@ -1,7 +1,24 @@
 import React, { Component } from 'react';
 
+import SearchBar from '../searchbar';
+
+import './index.css';
 export default class Header extends Component {
     render() {
-        return <div>I a=m the header</div>
+        return <div>
+            <header className="card card-body" id="header">
+                <div className="row">
+                    <div className="col-3">
+
+                    </div>
+                    <div className="col-5">
+                        <SearchBar />
+                    </div>
+                    <div className="col-4">
+                    </div>
+                </div>
+            </header>
+            <div style={{ marginTop: '3%' }}></div>
+        </div >
     }
 }
