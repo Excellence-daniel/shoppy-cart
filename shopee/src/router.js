e820bd9cef42d71124c185e83658e8dc71ed5fb0
Guest User <Guest@onyemaechismbp2.lan>
2019-04-17 13:50:56 +0100

    Reconstructed the react file, created a router and a header component that would be global across all pages and work would begin now

---
+++ shopee/src/router.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Header from './components/header'
+import App from './pages/index';
+
+export default function Routerr() {
+    return (
+        <Router>
+            <div>
+                <Header />
+                <Route exact path='/' component={App} />
+                {/* <Route exact path='/signup' component={SignUp} />
+                <Route exact path='/login' component={Login} />
+                <Route exact path='/account' component={Account} />
+                {/* <Route exact path = '/games' component = {Games}/> */}
+                {/* <Route exact path='/lotto' component={Lotto} /> */}
+                {/* <Route exact path='/confirmEmail' component={ConfirmEmail} /> */}
+                {/* <Route exact path='/checkResults' component={Result} /> */}
+
+                {/* <Route exact path = "/club/editClub" render ={(props) => <EditClub {...props}/>}/> */}
+            </div>
+        </Router>
+    )
+}
