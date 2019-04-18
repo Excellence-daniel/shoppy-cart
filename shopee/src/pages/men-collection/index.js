dbbd8791d93518aae54206ee232eba0e044790a8
Guest User <Guest@onyemaechismbp2.lan>
2019-04-18 13:17:53 +0100

    Created a page for component men's collection

---
+++ shopee/src/pages/men-collection/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class MenCollection extends Component {
+    render() {
+        return (
+            <div> Hey Man! </div>
+        )
+    }
+}
