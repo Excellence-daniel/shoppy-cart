df5111ee8594a0d463a77fc9e92dc224f81d381c
Daniel <eoyeniran@tradedept.co>
2019-05-11 18:14:04 +0100

    Redirected /admin to login for admins and also start working on the server that syncs with mongo

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const port = 4030;
 const mongodb = require('mongodb');
 // const MongoClient = mongodb.MongoClient;
-const mongoURL = 'mongodb+srv://daniel:excelly2000!@cluster0-bwf0v.mongodb.net/test?retryWrites=true';
+const mongoURL = 'mongodb+srv://DanielExcellence:excelly2000!@gettingstarted-ml8pi.mongodb.net/test?retryWrites=true';
 const bodyParser = require('body-parser');
 app.use(express.urlencoded({
     extended: true
@@ -31,17 +31,40 @@ client.connect((err, db) => {
 app.post('/addProduct', (req, res) => {
     console.log(req.body);
     if (req.body) {
-        MongoClient.connect(mongoURL, { useNewUrlParser: true }, function (err, db) {
-            // if (err) throw err;
-            console.log(db, err);
-            // var dbo = db.db("shop")  //declare the database to be used 
-            // var newObj = { name: 'Ade' }
-            // dbo.collection("products").insertOne(newObj, function (err, res) {
-            //     if (err) throw err;
-            //     console.log("Document inserted successfully.")
-            // })
-            // db.close()
-        })
+        const client = new MongoClient(mongoURL, { useNewUrlParser: true });
+        client.connect(async (err) => {
+            console.log('Heyy 2');
+            if (await err) throw err;
+
+            var newObj = { name: 'Ade' }
+            const collection = client.db("Products").collection("shops");
+            collection.insertOne(newObj, function (err, res) {
+                if (err) throw err;
+                console.log("Document inserted successfully.")
+            })
+            // if (err) { throw err };
+            // console.log('Hey');
+            // const collection = client.db("test").collection("devices");
+            // perform actions on the collection object
+            client.close();
+        });
+        // MongoClient.connect(mongoURL, { useNewUrlParser: true }, function (err, db) {
+        //     if (err) throw err;
+        //     // console.log(db, err);
+        //     // var dbo = db.db("shop")  //declare the database to be used 
+        //     // var newObj = { name: 'Ade' }
+        //     // dbo.collection("products").insertOne(newObj, function (err, res) {
+        //     //     if (err) throw err;
+        //     //     console.log("Document inserted successfully.")
+        //     // })
+        //     const collection = client.db("products").collection("shop");
+        //     collection.insertOne(newObj, function (err, res) {
+        //         if (err) throw err;
+        //         console.log("Document inserted successfully.")
+        //     })
+
+        //     // db.close()
+        // })
         // MongoClient.connect(mongoURL, { useNewUrlParser: true }, (err, db) => {
         //     if (err) throw err;
         //     console.log("Connected successfully to server");
