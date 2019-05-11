450990c331776ebf44313e421bb90dc871e8aba6
Daniel <eoyeniran@tradedept.co>
2019-05-11 22:34:39 +0100

    Wrote an axios post to a server path

--- shopee/src/pages/admin/signup.js
+++ shopee/src/pages/admin/signup.js
@@ -1,12 +1,52 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 import TextInput from './textField';
+import { server_database_url } from '../../global';
 
 export default class SignUp extends Component {
-    handleNameInput = () => {
+    constructor(props) {
+        super(props);
+        this.state = {
+            fullname: '',
+            email: '',
+            password: '',
+            agreed: false
+        }
+    }
+    handleNameInput = (e) => {
+        this.setState({ fullname: e.target.value.trim() });
+    }
 
+    handleEmailInput = (e) => {
+        console.log(e.target.value, 'email');
+        this.setState({ email: e.target.value.trim() });
+    }
+
+    handlePasswordInput = (e) => {
+        console.log(e.target.value, 'password');
+        this.setState({ password: e.target.value.trim() });
+    }
+
+    agreeFunc = () => {
+        const { agreed } = this.state;
+        console.log(!agreed)
+        this.setState({ agreed: !agreed });
+        // console.log(this.state.agreed)
+    }
+
+    handleAdminSignUp = async () => {
+        const { email, fullname, password, agreed } = this.state;
+        if (email !== '' && fullname !== '' && password !== '' && agreed === true) {
+            const url = `${server_database_url}/signup`;
+            const body = { fullname, email, password };
+            const signAdminUp = await axios.post(url, body);
+            console.log(signAdminUp, 'signup');
+        } else {
+            alert('Fill in all fields');
+        }
     }
     render() {
         return (
@@ -19,18 +59,18 @@ export default class SignUp extends Component {
                         </p>
 
                         <p className="email">
-                            <TextInput label={'Email Address'} icon={'fas fa-at'} function={this.handleNameInput} />
+                            <TextInput label={'Email Address'} icon={'fas fa-at'} function={this.handleEmailInput} />
                         </p>
 
                         <p className="password">
-                            <TextInput label={'Password'} icon={'fas fa-key'} function={this.handleNameInput} />
+                            <TextInput label={'Password'} icon={'fas fa-key'} function={this.handlePasswordInput} />
                         </p>
                         <p style={{ marginLeft: '3%' }}>
-                            <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+                            <input type="checkbox" class="form-check-input" id="exampleCheck1" onClick={this.agreeFunc} />
                             <label class="form-check-label" for="exampleCheck1">I agree with all <Link>Terms and Conditions</Link></label>
                         </p>
                         <p>
-                            <Button variant="contained" color="secondary" className="btn-block">
+                            <Button variant="contained" color="secondary" className="btn-block" onClick={this.handleAdminSignUp}>
                                 Admin SignUp
                         </Button>
                         </p>
