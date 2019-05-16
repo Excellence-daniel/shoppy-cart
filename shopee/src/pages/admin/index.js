79b87de62c29b36a82722903c85c73f418098c58
Daniel <eoyeniran@tradedept.co>
2019-05-16 16:32:57 +0100

    Module install and added a new router for admin : /logAdmin

--- shopee/src/pages/admin/index.js
+++ shopee/src/pages/admin/index.js
@@ -1,22 +1,22 @@
-import React, { Component } from 'react';
-import { Redirect } from 'react-router-dom';
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
 
 export default class Admin extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            redirect: false
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      redirect: false
+    };
+  }
 
-    componentDidMount = () => {
-        this.setState({ redirect: true });
+  componentDidMount = () => {
+    this.setState({ redirect: true });
+  };
+  render() {
+    const { redirect } = this.state;
+    if (redirect) {
+      return <Redirect to="/login" />;
     }
-    render() {
-        const { redirect } = this.state;
-        if (redirect) {
-            // return <Redirect to="/login" />
-        }
-        return <div></div>
-    }
+    return <div />;
+  }
 }
